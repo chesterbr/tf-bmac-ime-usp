bc30a6ba4a1e5e726877caaf3145682e5b4b63a1
chesterbr <cd@pobox.com>
2008-12-28 16:36:13 +0000

    Melhorado interpretador de matrizes - agora ele ignora espaços e linhas em branco extras, permitindo a múltipla execução de passos sobre a mesma matriz

--- code/tf/src/main/java/tf/helpers/MatrixHelper.java
+++ code/tf/src/main/java/tf/helpers/MatrixHelper.java
@@ -1,5 +1,7 @@
 package tf.helpers;
 
+import java.util.ArrayList;
+
 import Jama.Matrix;
 
 public class MatrixHelper {
@@ -8,18 +10,24 @@ public class MatrixHelper {
 		String[] linhas = s.split("\\n");
 		double[][] elementos = new double[linhas.length][];
 		int l = 0;
-		for(String linha: linhas) {
-			String[] colunas = linha.split("[\\n\\t ]");
-			elementos[l] = new double[colunas.length];
-			int c = 0;
-			for (String elemento: colunas) {
-				elementos[l][c] = Double.parseDouble(elemento);
-				c++;
+		for (String linha : linhas) {
+			System.out.println("CHESTER:linha:[" + linha + "]");
+			String[] colunas = linha.split("[\\n\\t ]+");
+			ArrayList<Double> al = new ArrayList<Double>();
+			for (String elemento : colunas) {
+				System.out.println("CHESTER:elemento:[" + elemento + "]");
+				if (elemento.trim().length() > 0) {
+					al.add(Double.parseDouble(elemento));
+				}
 			}
+			elementos[l] = new double[al.size()];
+			for (int i = 0; i < al.size(); i++)
+				elementos[l][i] = al.get(i);
+
 			l++;
 		}
 		// TODO lançar exceção amigável se der problema aqui
 		return new Matrix(elementos);
 	}
-	
+
 }
